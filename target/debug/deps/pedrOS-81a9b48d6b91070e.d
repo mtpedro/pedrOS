c515c49b61e8677ce708fde97342f9c2bff91e63
hugo <hugo@moniker.net>
2022-09-12 19:41:53 +1000

    cleanup

--- target/debug/deps/pedrOS-81a9b48d6b91070e.d
+++
@@ -1,5 +0,0 @@
-/Users/hugomayo/Documents/projects/code/rust/prophecy_os/target/debug/deps/pedrOS-81a9b48d6b91070e.rmeta: src/main.rs
-
-/Users/hugomayo/Documents/projects/code/rust/prophecy_os/target/debug/deps/pedrOS-81a9b48d6b91070e.d: src/main.rs
-
-src/main.rs:
